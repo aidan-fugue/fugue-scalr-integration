4470e747f824b591d97788f4f1a56463ede8acc8
aidan <aidan@fugue.co>
2021-11-13 23:27:18 -0500

    dropping exit code things

--- scripts/before-plan.sh
+++ scripts/before-plan.sh
@@ -5,9 +5,9 @@ echo ""
 tput setaf 4; echo "Checking your terraform for validity and CIS Benchmark compliance with regula."
 echo ""
 if [$regula_exit_code == 3 ] && [$tf_validation_code == "Success! The configuration is valid." ]; then
-  tput setaf 2; echo "Terraform is valid and compliant."; exit 0
+  tput setaf 2; echo "Terraform is valid and compliant.";
 else
   tput setaf 1; echo "Error! Terraform Apply failed and/or regula run failed."
   tput setaf 1; echo "Recommend running regula run and terraform validate again and fixing any errors you get."
-  echo ""; exit 1
+  echo "";
 fi
