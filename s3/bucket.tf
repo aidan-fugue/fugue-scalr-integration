ee0c121a3fcf9e56c943d1e3c9254f54935ea5d6
aidan <aidan@fugue.co>
2021-11-13 23:02:36 -0500

    updating for bucket name issue

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "demos3" {
-    bucket      = "${var.bucket_name}"
+    bucket      = "my-scalr-fugue-test-bucket-2731"
     acl         = "${var.acl_value}"
 }
--- s3/var.tf
+++ s3/var.tf
@@ -1,7 +1,3 @@
-variable "bucket_name" {
-    bucket_name = "my-scalr-fugue-test-bucket-2731"
-}
-
 variable "acl_value" {
     default = "private"
 }
