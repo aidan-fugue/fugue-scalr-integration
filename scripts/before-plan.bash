f5329dbd570a3abf560a4ff8823c317d47983696
aidan <aidan@fugue.co>
2021-11-16 15:55:33 -0500

    changing the pre plan script

--- scripts/before-plan.bash
+++ scripts/before-plan.bash
@@ -2,12 +2,12 @@
 regula_exit_code=$(docker run --rm -t -v $(pwd):/workspace fugue/regula:v2.0.1 run | wc -l)
 tf_validation_code=$(terraform validate)
 echo ""
-tput setaf 4; echo "Checking your terraform for validity and CIS Benchmark compliance with regula."
+echo "Checking your terraform for validity and CIS Benchmark compliance with regula."
 echo ""
-if [$regula_exit_code == 3 ] && [$tf_validation_code == "Success! The configuration is valid." ]; then
-  tput setaf 2; echo "Terraform is valid and compliant.";
+if [ $regula_exit_code == 3 ] && [ $tf_validation_code == "Success! The configuration is valid." ]; then
+  echo "Terraform is valid and compliant.";
 else
-  tput setaf 1; echo "Error! Terraform Apply failed and/or regula run failed."
-  tput setaf 1; echo "Recommend running regula run and terraform validate again and fixing any errors you get."
+  echo "Error! Terraform Apply failed and/or regula run failed."
+  echo "Recommend running regula run and terraform validate again and fixing any errors you get."
   echo "";
 fi
