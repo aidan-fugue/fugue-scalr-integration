c7197ae64f633020bb72901b3408331727e44763
aidan <aidan@fugue.co>
2021-11-16 15:36:49 -0500

    adding aws creds in a different way

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    region = "us-east-1"
-    shared_credentials_file = "/Users/aidanoconnor/.aws/credentials"
-    profile = "aidan_sandbox"
+    region                  = "us-east-1"
+    access_key              = "{var.aws_access_key_id}"
+    secret_key              = "{var.aws_secret_access_key}"
 }
 
 module "s3" {
