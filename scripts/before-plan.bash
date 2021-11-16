c1c85bf0a76b7f248a4dd64949970296751367f6
aidan <aidan@fugue.co>
2021-11-16 15:56:37 -0500

    adding exit codes

--- scripts/before-plan.bash
+++ scripts/before-plan.bash
@@ -5,9 +5,9 @@ echo ""
 echo "Checking your terraform for validity and CIS Benchmark compliance with regula."
 echo ""
 if [ $regula_exit_code == 3 ] && [ $tf_validation_code == "Success! The configuration is valid." ]; then
-  echo "Terraform is valid and compliant.";
+  echo "Terraform is valid and compliant."; exit 0
 else
   echo "Error! Terraform Apply failed and/or regula run failed."
   echo "Recommend running regula run and terraform validate again and fixing any errors you get."
-  echo "";
+  echo ""; exit 1
 fi
