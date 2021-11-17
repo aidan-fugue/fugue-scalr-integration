576f255567aaad6c1dcc71e8781d7063daa76cd7
aidan <aidan@fugue.co>
2021-11-17 10:01:15 -0500

    improved text output appearance

--- scripts/before-plan.bash
+++ scripts/before-plan.bash
@@ -3,12 +3,12 @@
 #the security and compliance of the terraform according to CIS Benchmarks
 #with Regula (https://regula.dev/).
 
-#Setting variables: length of regula run; terraform validate output; red and green text
+#Setting variables: length of regula run and terraform validate output
 regula_exit_code=$(./regula run | wc -l)
 tf_validation_code=$(terraform validate)
 
 #Initial run message to tell me that the script it checking my terraform
-echo "Checking your terraform for validity and CIS Benchmark compliance with regula."
+echo "Checking your terraform for validity and CIS Benchmark compliance with regula..."
 echo ""
 
 #logic to check for validity and, if not valid, throw an error and non-zero exit code
@@ -16,9 +16,6 @@ if [ $regula_exit_code == 3 ] && [ $tf_validation_code == "Success! The configur
   echo "Terraform is valid and compliant."; exit 0
 else
   echo "Error! Terraform Apply failed and/or regula run failed."
-  echo "Recommend running regula run and terraform validate again and fixing any errors you get."
-  echo ""
-  echo "See below for the errors we have detected:"
   echo ""
   echo "Security and compliance errors:"
   echo ""
