0022e8d2aebf99d5363cb5ce81b977ac5ab572c0
aidan <aidan@fugue.co>
2021-11-17 14:20:19 -0500

    adjusting after-plan basg script

--- scripts/after-plan.bash
+++ scripts/after-plan.bash
@@ -4,8 +4,8 @@ FILES=$(find . -type f -name "*.tf")
 for f in $FILES
 do
     echo ""
-    echo "Adjust the formatting of $f per HCL canonical standard..."
-    #terraform fmt $f
+    echo "Adjusting the formatting of $f per HCL canonical standard..."
+    terraform fmt $f
     echo "Done formatting $f!"
     echo ""
 done
