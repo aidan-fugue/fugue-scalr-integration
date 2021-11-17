4002690cb2feb82cc5b3cea820f7c748bb618cad
aidan <aidan@fugue.co>
2021-11-17 14:17:55 -0500

    fixing bash script take 101

--- scripts/before-plan.bash
+++ scripts/before-plan.bash
@@ -5,14 +5,14 @@
 
 #Setting variables: length of regula run and terraform validate output
 regula_exit_code=$(./regula run --exclude FG_R00275 | wc -l)
-tf_validation_code=$(terraform validate)
+tf_validation_code=$(terraform validate | wc -l)
 
 #Initial run message to tell me that the script it checking my terraform
 echo "Checking your terraform for validity and CIS Benchmark compliance with regula..."
 echo ""
 
 #logic to check for validity and, if not valid, throw an error and non-zero exit code
-if [ $regula_exit_code == 2 ] && "$tf_validation_code" == "Success! The configuration is valid."; then
+if [ $regula_exit_code == 2 ] && [ "$tf_validation_code" == 2 ]; then
   echo "Terraform is valid and compliant."; exit 0
 else
   echo "Error! Terraform Apply failed and/or regula run failed."
