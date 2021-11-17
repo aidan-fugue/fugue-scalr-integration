7f2fcf0b15618ee9e997de60b37f4de251557a4a
aidan <aidan@fugue.co>
2021-11-17 13:08:46 -0500

    trying something new with regula run variable take 4

--- scripts/before-plan.bash
+++ scripts/before-plan.bash
@@ -4,8 +4,8 @@
 #with Regula (https://regula.dev/).
 
 #Setting variables: length of regula run and terraform validate output
-a=$(./regula run --exclude FG_R00275)
-arr=($a)
+a=$(regula run --exclude FG_R00275)
+arr=("$a")
 tf_validation_code=$(terraform validate)
 
 #Initial run message to tell me that the script it checking my terraform
@@ -13,14 +13,14 @@ echo "Checking your terraform for validity and CIS Benchmark compliance with reg
 echo ""
 
 #logic to check for validity and, if not valid, throw an error and non-zero exit code
-if [ "${arr[@]:0:3}" == "No problems found." ] && [ "$tf_validation_code" == "Success! The configuration is valid." ]; then
+if  "${arr[@]:0:3}" == "No problems found." && "$tf_validation_code" == "Success! The configuration is valid."; then
   echo "Terraform is valid and compliant."; exit 0
 else
   echo "Error! Terraform Apply failed and/or regula run failed."
   echo ""
   echo "Security and compliance errors:"
   echo ""
-  ./regula run --exclude FG_R00275
+  regula run --exclude FG_R00275
   echo "Terraform errors:"
   echo ""
   terraform validate
