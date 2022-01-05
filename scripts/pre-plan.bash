e9341cb4d74663f86b1c57957c8512a5f514790d
aidan <aidan@fugue.co>
2022-01-05 10:27:11 -0500

    modified script

--- scripts/pre-plan.bash
+++ scripts/pre-plan.bash
@@ -12,7 +12,7 @@ tar -zxvf regula_2.3.0_Linux_x86_64.tar.gz -O > regula
 echo ""
 echo "Modifying Regula executable"
 echo ""
-chmod 755 regula/regula
+chmod u+x regula
 
 echo ""
 echo "Working directory:"
@@ -26,4 +26,4 @@ echo ""
 echo "Run Regula"
 echo ""
 regula run *.tf
-/opt/workdir/s3/regula/regula run ../*/tf
+regula run ../*/tf
