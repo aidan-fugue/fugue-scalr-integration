08c8be5f09d7af1d75983ec90bd43cfd47552b2a
aidan <aidan@fugue.co>
2022-01-05 12:12:14 -0500

    trying something new

--- scripts/pre-plan.bash
+++ scripts/pre-plan.bash
@@ -1,16 +1,21 @@
 #!/bin/bash
+
 #Setting color variables for outputs
 green='\033[0;32m'
-clear='\033[0m'
 red='\033[0;31m'
 
 echo "Downloading latest version of Regula..."
 echo ""
-wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
+
+#wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
+wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*_Linux_x86_64.tar.gz' -o)
+
 #Extracting Regula
 tar -zxf regula_2.3.0_Linux_x86_64.tar.gz regula
+
 echo ""
 echo -e "${green}Running Regula on your Terraform...${clear}"
-#Running Regula
+echo ""
+
 cd ../
 ./regula run
