cb89326d529cd8df9df535c92aa5df149d225eac
aidan <aidan@fugue.co>
2022-01-05 14:24:17 -0500

    updated script

--- scripts/pre-plan.bash
+++ scripts/pre-plan.bash
@@ -4,7 +4,7 @@
 green='\033[0;32m'
 clear='\033[0m'
 
-echo "Downloading latest version of Regula..."
+echo "Downloading the latest version of Regula from https://github.com/fugue/regula/releases/latest..."
 echo ""
 wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*_Linux_x86_64.tar.gz' -o)
 
@@ -12,7 +12,10 @@ wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releas
 tar -zxf regula_2.3.0_Linux_x86_64.tar.gz regula
 
 echo ""
-echo -e "${green}Running Regula on your Terraform...${clear}"
+echo -e "${green}Scanning the security and compliance of your Terraform against CIS Benchmarks with Regula...${clear}"
+echo ""
+echo -e "${green}Would you like access to more compliance families (such as HIPAA, SOC2, NIST 800-53, CSA, GDPR, ISO 27001, and PCI DSS)?${clear}"
+echo -e "${green}Email sales@fugue.co or visit https://www.fugue.co/${clear}"
 echo ""
 cd ../
-./regula run -s critical
+./regula run
