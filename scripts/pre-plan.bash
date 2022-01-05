6bcf347888678f8b8a1fbd0c4392cd3d1dc4ee2a
aidan <aidan@fugue.co>
2022-01-05 11:04:49 -0500

    trying something new

--- scripts/pre-plan.bash
+++ scripts/pre-plan.bash
@@ -2,7 +2,7 @@
 echo "Downloading latest version of Regula..."
 echo ""
 
-wget -c https://github.com/$(wget https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o) -nv
+wget -c https://github.com/$(wget https://github.com/fugue/regula/releases/latest -O - -nv | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o -nv) -nv
 
 echo ""
 echo "Extracting Regula"
