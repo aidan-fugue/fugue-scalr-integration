c9001deb44dc887a51dc8adb228866fd7729f2e9
aidan <aidan@fugue.co>
2022-01-05 11:56:07 -0500

    trying something new

--- scripts/pre-plan.bash
+++ scripts/pre-plan.bash
@@ -5,7 +5,7 @@ echo ""
 #wget -c https://github.com/$(wget https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
 #wget -c -nv https://github.com/$(wget https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
 #wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
-wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o -q)
+wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
 #Extracting Regula
 tar -zxf regula_2.3.0_Linux_x86_64.tar.gz regula
 
