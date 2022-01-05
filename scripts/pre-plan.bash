cae6765ee4a8bb71dd2af9afdca2b1b538a7ee72
aidan <aidan@fugue.co>
2022-01-05 11:35:07 -0500

    trying something new

--- scripts/pre-plan.bash
+++ scripts/pre-plan.bash
@@ -1,19 +1,14 @@
 #!/bin/bash
 echo "Downloading latest version of Regula..."
 echo ""
-
-wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
+wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep -q '/.*/.*/.*regula_2.3.0_Linux_x86_64.tar.gz' -o)
 
 #Extracting Regula
 tar -zxf regula_2.3.0_Linux_x86_64.tar.gz regula
 
-echo ""
-echo "Modifying Regula executable!"
-echo ""
+#Modifying Regula executable
 chmod u+x regula
 
-echo ""
-echo "Run Regula"
-echo ""
+#Changing directores and running Regula
 cd ../
 ./regula run
