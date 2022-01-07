8aa832401ad4b3270e869d5e5b76359a4fd39fc8
aidan <aidan@fugue.co>
2022-01-07 11:24:53 -0500

    updated bash script

--- scripts/pre-plan.bash
+++ scripts/pre-plan.bash
@@ -1,21 +1,33 @@
 #!/bin/bash
+#This script scans Terraform for security & compliance violations;
+#and HCL formatting & logic errors. This script should be used as
+#the Scalr "Before plan" custom hook.
 
 #Setting color variables for outputs
 green='\033[0;32m'
 clear='\033[0m'
 
-echo "Downloading the latest version of Regula from https://github.com/fugue/regula/releases/latest..."
-echo ""
-wget -c -nv https://github.com/$(wget -nv https://github.com/fugue/regula/releases/latest -O - | egrep '/.*/.*/.*_Linux_x86_64.tar.gz' -o)
+#Downloading the latest version of Regula
+wget -c -nv https://github.com/$(wget -nv \
+https://github.com/fugue/regula/releases/latest -O - | \
+egrep '/.*/.*/.*_Linux_x86_64.tar.gz' -o)
 
 #Extracting Regula
 tar -zxf regula_2.3.0_Linux_x86_64.tar.gz regula
 
-echo ""
+#Scanning for CIS Benchmark security and compliance violations
 echo -e "${green}Scanning the security and compliance of your Terraform against CIS Benchmarks with Regula...${clear}"
-echo ""
-echo -e "${green}Would you like access to more compliance families (such as HIPAA, SOC2, NIST 800-53, CSA, GDPR, ISO 27001, and PCI DSS)?${clear}"
-echo -e "${green}Email sales@fugue.co or visit https://www.fugue.co/${clear}"
-echo ""
 cd ../
-./regula run
+./regula run --include waivers.rego
+
+echo -e "${green}Validating and fixing the formatting of your Terraform...${clear}"
+#Adjusting Terraform formatting to match HCL canonical standard
+terraform fmt
+
+#Validating Terraform
+terraform validate
+
+#(Optional) Send results of security and compliance scan to Fugue UI
+#./regula run --sync --upload
+
+echo -e "${green}Security. compliance, validation, and formatting checks are complete!${clear}"
