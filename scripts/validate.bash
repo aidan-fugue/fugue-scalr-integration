b5f6a67d9421a6a282ec38b27db8d34577b04ade
aidan <aidan@fugue.co>
2022-01-07 15:33:14 -0500

    new script format

--- scripts/validate.bash
+++ scripts/validate.bash
@@ -4,13 +4,16 @@
 #Setting color variables for outputs
 green='\033[0;32m'
 clear='\033[0m'
+red='\033[0;31m'
+bold=$(tput bold)
+norm=$(tput sgr0)
 
-echo -e "${green}Validating and fixing the formatting of your Terraform...${clear}"
+echo -e "\n${green}${bold}Validating and fixing the formatting of your Terraform...${clear}${norm}\n"
 terraform init && terraform fmt && terraform validate
 
 rc=$?
 if [[ $rc != 0 ]]; then
-    echo "Please fix the Terraform issues identified above, then re-run the Scalr pipeline.";
+    echo -e "\n${red}${bold}Please fix the Terraform issues identified above, then re-run the Scalr pipeline.${clear}${norm}\n";
   else 
-    echo "Terraform checks passed successfully!"
+    echo -e "${green}${bold}\nTerraform checks passed successfully!${clear}${norm}\n"
   fi
